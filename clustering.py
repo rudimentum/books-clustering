44822ab4223e192a8191ff0941640c912aec5a01
Victoria Melnikova <31312528+ramquist@users.noreply.github.com>
2020-03-22 13:30:06 +0300

    add 3d model Axes3D

--- clustering.py
+++ clustering.py
@@ -2,6 +2,8 @@ from sklearn.cluster import KMeans
 import numpy as np
 #2d model
 import matplotlib.pyplot as plt
+#3d model
+from mpl_toolkits.mplot3d import Axes3D
 
 # words count, reviews count, year
 data = np.array([ 
@@ -106,5 +108,14 @@ for cluster in range(N_CLUSTERS):
 plt.figure(figsize=(6,4))
 plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
 plt.scatter(data[:,0], data[:,1], c= kmeans.labels_.astype(float))
-plt.title('K-Means Clustering')
+plt.title('K-Means Clustering 2D')
+plt.show()
+
+#3d model
+fig = plt.figure() 
+ax = Axes3D(fig)
+
+ax.scatter(centroids[:, 0], centroids[:, 1], centroids[:, 2], c='red', s=50)
+ax.scatter(data[:,0], data[:,1], data[:,2], c= kmeans.labels_.astype(float))
+ax.set_title('K-Means Clustering 3D')
 plt.show()
